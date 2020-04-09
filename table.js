d7b014b013850e91fb60e1754ac1832fdd944939
DoParkEQ <do.park@eqworks.com>
2020-04-09 11:15:37 -0400

    Style enhancement: modified CSS property of the dashboard - including font size and weight, icons, spacing, colors, etc

---
+++ table.js
@@ -0,0 +1,161 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import TableSortLabel from '@material-ui/core/TableSortLabel'
+import Paper from '@material-ui/core/Paper'
+
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1
+  }
+  return 0
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy)
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index])
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0])
+    if (order !== 0) return order
+    return a[1] - b[1]
+  })
+  return stabilizedThis.map((el) => el[0])
+}
+
+const headCells = [
+  { id: 'name', numeric: false, disablePadding: false, label: 'Province' },
+  { id: 'total', numeric: true, disablePadding: false, label: 'Total' },
+  { id: 'newCases', numeric: true, disablePadding: false, label: 'New' },
+  { id: 'rate', numeric: true, disablePadding: false, label: 'Increase Rate' },
+]
+
+function EnhancedTableHead(props) {
+  const { classes, order, orderBy, onRequestSort } = props
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property)
+  }
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'center' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+            className={classes.head}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  )
+}
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    maxHeight: '264px',
+    width: '100%'
+  },
+  root: { width: '100%' },
+  table: { minWidth: '100%' },
+  head: { fontSize: theme.typography.subtitle1 },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}))
+
+export default function EnhancedTable({data}) {
+  const classes = useStyles()
+  const [order, setOrder] = React.useState('asc')
+  const [orderBy, setOrderBy] = React.useState('total')
+  const rows = data.map((row) => ({
+      name: row.province,
+      total: row.total_cases,
+      newCases: row.new_cases,
+      rate: Math.round(row.new_cases / (row.total_cases - row.new_cases) * 100)
+    })
+  )
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc'
+    setOrder(isAsc ? 'desc' : 'asc')
+    setOrderBy(property)
+  }
+
+  return (
+      <Paper className={classes.root} elevation={0}>
+        <TableContainer className={classes.container}>
+          <Table
+            stickyHeader
+            className={classes.table}
+            aria-labelledby='tableTitle'
+            size='medium'
+            aria-label='enhanced table'
+          >
+            <EnhancedTableHead
+              classes={classes}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`
+
+                  return (
+                    <TableRow
+                      hover={true}
+                      tabIndex={-1}
+                      key={row.name}
+                    >
+                      <TableCell component='th' id={labelId} scope='row' >
+                        {row.name}
+                      </TableCell>
+                      <TableCell align='center'>{row.total.toLocaleString()}</TableCell>
+                      <TableCell align='center'>{row.newCases.toLocaleString()}</TableCell>
+                      <TableCell align='center'>{row.rate}%</TableCell>
+                    </TableRow>
+                  )
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+  )
+}
