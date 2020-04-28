9280260e995636d8261791d57d3adb26470d210e
DoParkEQ <do.park@eqworks.com>
2020-04-28 12:08:29 -0400

    Table component rewritten: it is now responsive to incoming data

--- custom-table.js
+++ custom-table.js
@@ -1,60 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { Table, TableHead, TableRow, TableBody, TableContainer, TableCell} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { findByLabelText } from '@testing-library/react';
+import React, { useState, useEffect } from 'react'
+import {Icon, Table, TableHead, TableRow, TableBody, TableContainer, TableCell} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import UnfoldMoreIcon from '@material-ui/icons/UnfoldMore'
+
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        width: '100%'
-    },
+    root: {width: '100%'},
     head: {
-        display: 'flex',
-        flexGrow: 1
+        fontSize: 'body',
+        fontWeight: 600
     },
-    cell: {
-        flexGrow: 1
-    }
+    cell: {flexGrow: 1}
 }))
 
-const CustomTable = ({data}) => {
-    const [dynamicData, setDynamicData] = useState(data);
-    const tableHead = Object.getOwnPropertyNames(data[0]);
-    const classes = useStyles();
+const nameReplacer = name => {
+    return name.charAt(0).toUpperCase() + name.slice(1).replace(/_/g, ' ')
+}
 
+const CustomTable = ({data}) => {
+    const classes = useStyles()
+
+    const [dynamicData, setDynamicData] = useState(data)
+    const [order, setOrder] = useState(false)
+    const tableHead = Object.getOwnPropertyNames(data[0])
 
     const dynamicSort = name => {
-        const sorted = dynamicData
+        setOrder(!order)
+        const sorted = [...dynamicData]
         .sort((a, b) => {
-            console.log(a[name],b[name]);
-            if(a[name] < b[name]) {
-                return -1;
+            if(order ? a[name] < b[name] : a[name] > b[name]) {
+                return -1
             }
-            if(a[name] > b[name]) {
-                return 1;
+            if(order ? a[name] > b[name] : a[name] < b[name]) {
+                return 1
             }
-            return 0;
-        });
-        setDynamicData(sorted);
+            return 0
+        })
+        setDynamicData(sorted)
     }
-    
+
+    const headList = tableHead.map(name => 
+        <TableCell className={classes.head} onClick={()=>dynamicSort(name)}>{nameReplacer(name)}</TableCell>)
+
+    const bodyList = dynamicData.map(row => (
+        <TableRow>
+            {Object.values(row).map(column => <TableCell>{column}</TableCell>)}
+        </TableRow>
+    ))
+
     return (
         <TableContainer className={classes.root}>
             <Table stickyHeader>
                 <TableHead >
-                    <TableRow>
-                    {tableHead.map(name => <TableCell onClick={()=>dynamicSort(name)}>{name}</TableCell>)}
-                    </TableRow>
+                    <TableRow>{headList}</TableRow>
                 </TableHead>
-                <TableBody>
-                    {dynamicData.map(row => (
-                        <TableRow>
-                            {Object.values(row).map(column => <TableCell>{column}</TableCell>)}
-                        </TableRow>
-                    ))}
-                </TableBody>
+                <TableBody>{bodyList}</TableBody>
             </Table>
         </TableContainer>
-    );
-};
+    )
+}
 
-export default CustomTable;
+export default CustomTable
