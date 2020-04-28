bf97fd5dce685c81c66db3e5bc8edcee6b6f2498
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-04-28 13:34:34 -0400

    DataTable - add propTypes and map keys

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-export { default as WidgetTrend } from "./widget-trend"
+export { default as WidgetTrend } from './widget-trend'
 export { default as WidgetNumber } from './widget-number'
+export { default as DataTable } from './data-table'
