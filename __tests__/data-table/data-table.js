a6f872cd3b3407206c70b78f4651278f66bcf37a
Gulsah Sarsilmaz <gulsah.sarsilmaz@eqworks.com>
2020-05-19 13:24:01 -0400

    Added Additional unit tests to data table and widget for validity

--- __tests__/data-table/data-table.js
+++ __tests__/data-table/data-table.js
@@ -3,6 +3,17 @@ import renderer from 'react-test-renderer'
 import DataTable from '../../src/data-table';
 
 
+beforeEach(() => {
+  jest.spyOn(console, 'error')
+  jest.spyOn(console, 'warn')
+})
+
+afterEach(() => {
+  /* eslint-disable no-console */
+  expect(console.error).not.toBeCalled()
+  expect(console.warn).not.toBeCalled()
+})
+
 describe('<DataTable/>', () => {
   it('should render successfully', async () => {
     let data = {
@@ -16,5 +27,31 @@ describe('<DataTable/>', () => {
 
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  // Default Props - Missing Cases
+  it.each([
+    {}, // empty
+    { data: [] }, // missing isPercentage
+    { isPercentage: false }, // missing data
+  ])('Checking failing case for %p', (props) => {
+    const component = renderer.create(
+      <DataTable {...props} />
+    )
+    const componentInstance = component.root
+    expect(componentInstance.props).not.toEqual(props)
+  })
+
+  // Default Props - Valid Cases
+  it.each([
+    { data: [], isPercentage: true }, // data: checking empty array, isPercentage: non-default prop value
+    { data: [], isPercentage: false }, // data: checking empty array, isPercentage: default prop value
+    { data: [{}], isPercentage: false }, // data: checking array of obj
+  ])('Checking passing case for %p', (props) => {
+    const component = renderer.create(
+      <DataTable {...props} />
+    )
+    const componentInstance = component.root
+    expect(componentInstance.props).toStrictEqual(props)
   })
 })
