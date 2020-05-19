8f56834a485557561101334f24ce1353b9e58bcb
DoParkEQ <do.park@eqworks.com>
2020-05-19 16:44:37 -0400

    update - theme added

---
+++ src/Theme/palette.js
@@ -0,0 +1,25 @@
+const white = "#FFFFFF";
+const black = "#000000";
+const paletteLight = {
+  type: "light",
+  white,
+  black,
+  primary: { main: "#0084ff" },
+  secondary: { main: "#9e9e9e" },
+  error: { main: "#ff0000" },
+  warning: { main: "#ffd800" },
+  info: { main: "#0076ff" },
+  success: { main: "#00de11" },
+  text: {
+    primary: black,
+    secondary: "rgba(0, 0, 0, 0.54)",
+    link: "#0076ff",
+  },
+  background: {
+    highlight: "#f2f2f2",
+    paper: white,
+    default: white,
+  },
+};
+
+export default paletteLight;
