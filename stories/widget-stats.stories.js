eb1b7222c3a6c902a08b80820cb568d92479bcc6
Maliha Hossain <malu.hoss@gmail.com>
2020-05-29 14:42:08 -0400

    Widget Stats: Added story to reflect increase showing as red arrow

--- stories/widget-stats.stories.js
+++ stories/widget-stats.stories.js
@@ -43,6 +43,20 @@ storiesOf('WidgetStats', module)
       }}
     />
   ))
+  .add('isPercentage (increase is red)', () => (
+    <WidgetStats
+      title='Example Trend'
+      value={(Math.random() * (100 - 1) + 1).toFixed(0)}
+      prev={(Math.random() * (100 - 1) + 1).toFixed(0)}
+      trendInfo={{
+        isTrendPercentage: true,
+        upIsGreen: false,
+        up: 'increase',
+        down: 'decrease',
+        comparedTo: 'from yesterday'
+      }}
+    />
+  ))
   .add('Children', () => (
     <WidgetStats
       title='This Month'
