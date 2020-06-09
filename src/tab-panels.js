55ee4bb3e3cc82a7a8f3edb43338743160119a9c
hyx131 <selin.hyx@gmail.com>
2020-06-09 13:40:32 -0400

    tabs - enable custom styling to tab panels

--- src/tab-panels.js
+++ src/tab-panels.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
 import { Tab, Tabs} from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 
 
 const TabPanel = ({ children, value, index }) => value === index && children
 
-const TabPanels = ({ tabIndex=0, tabLabels, tabChildren }) => {
+const TabPanels = ({ tabIndex, tabLabels, tabChildren, customStyles, customTabs, customTab }) => {
   const [value , setValue] = useState(tabIndex)
   const onTabChange = (_, newVal) => setValue(newVal)
+  let TabsComponent = Tabs
+  let TabComponent = Tab
+
+  if (customStyles) {
+    TabsComponent = withStyles(customTabs)(Tabs)
+    TabComponent = withStyles(customTab)((props) => <Tab disableRipple {...props} />)
+  }
 
   return (
     <>
-      <Tabs value={value} onChange={onTabChange}>
-        {tabLabels.length > 0 && tabLabels.map(label => <Tab key={label} label={label} />)}
-      </Tabs>
+      <TabsComponent value={value} onChange={onTabChange}>
+        {tabLabels.length > 0 && tabLabels.map(label => <TabComponent key={label} label={label} />)}
+      </TabsComponent>
 
       {tabChildren.length > 0 && tabChildren.map((child, i) => <TabPanel key={i} value={value} index={i}>{child}</TabPanel>)}
     </>
@@ -24,10 +32,14 @@ TabPanels.propTypes = {
   tabIndex: PropTypes.number,
   tabLabels: PropTypes.array,
   tabChildren: PropTypes.array,
+  customStyles: PropTypes.bool,
+  customTabs: PropTypes.func,
+  customTab: PropTypes.func,
 }
 
 TabPanels.defaultProps = {
   tabIndex: 0,
+  customStyles: false,
 }
 
 export default TabPanels
