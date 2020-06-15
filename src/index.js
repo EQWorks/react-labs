7e62ca1df5704fdf85fba44583728ac4d442bc1b
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-06-15 13:20:25 -0400

    README - update import list of root level components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-export { default as WidgetTrend } from './widget-trend' // deprecated
-export { default as WidgetNumber } from './widget-number' // deprecated
+export { default as WidgetTrend } from './widget-trend' // deprecated by WidgetStats
+export { default as WidgetNumber } from './widget-number' // deprecated by WidgetStats
 export { default as WidgetStats } from './widget-stats'
-export { default as DataTable } from './data-table'
+export { default as DataTable } from './data-table' // deprecated by Table
 export { default as QuickFilters } from './quick-filters'
 export { default as DynamicButton } from './dynamic-button'
-export { default as DynamicDataTable } from './dynamic-data-table'
+export { default as DynamicDataTable } from './dynamic-data-table' // deprecated by Table
 export { default as Table } from './table'
