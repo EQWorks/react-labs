e82f9323a03b0cad9777d8857a1d14d0a828efd0
DoParkEQ <do.park@eqworks.com>
2020-06-17 20:28:52 -0400

    theme - props augmentation

--- src/themes/palette.js
+++ src/themes/palette.js
@@ -1,37 +1,29 @@
-const locusBlue = {
-  50:  '#E2F3FF',
-  100: '#BAE0FF',
-  200: '#8BCEFF',
-  300: '#53BAFF',
-  400: '#14AAFF',
-  500: '#009AFF',
-  600: '#008BFF',
-  700: '#0075FF',
-  800: '#1564EC',
-  900: '#2242CD',
-};
+import { fade } from '@material-ui/core/styles/colorManipulator';
 
 const palette = {
-  type: "light",
-  primary: { main: locusBlue[700] },
+  primary: { main: '#0075FF' },
   error: { main: "#ea0000" },
   warning: { main: "#f4b000" },
   info: { main: "#741fff" },
   success: { main: "#00d308" },
-  text: {
-    primary: "#000",
-    secondary: "rgba(0, 0, 0, 0.54)",
-    link: "#0076ff",
+  shade: {
+    primary: {
+      50:  '#E2F3FF',
+      100: '#BAE0FF',
+      200: '#8BCEFF',
+      300: '#53BAFF',
+      400: '#14AAFF',
+      500: '#009AFF',
+      600: '#008BFF',
+      700: '#0075FF',
+      800: '#1564EC',
+      900: '#2242CD',
+    },
   },
-  background: {
-    highlight: "#f2f2f2",
-    paper: "#fff",
-    default: "#fff",
+  state: {
+    hoverWhite: () => fade(this.primary.main, 0.05),
+    hoverColored: 'rgba(0,0,0,0.15)',
   },
-  disabled: "rgba(0, 0, 0, 0.26)",
-  hoverOnWhite: '#F4F6FD',
-  hoverOnPrimary: '#115CE6',
-  locusBlue,
 };
 
 export default palette;
