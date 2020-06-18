ee30bfbdebb5fa6b221cdcdc5bee06d02b56fa94
DoParkEQ <do.park@eqworks.com>
2020-06-17 20:47:01 -0400

    theme - shadow props added

--- src/themes/palette.js
+++ src/themes/palette.js
@@ -24,6 +24,12 @@ const palette = {
     hoverWhite: () => fade(this.primary.main, 0.05),
     hoverColored: 'rgba(0,0,0,0.15)',
   },
+  shadow: {
+    10: '0px 1px 4px rgba(12, 12, 13, 0.1)',
+    20: '0px 2px 8px rgba(12, 12, 13, 0.15)',
+    30: '0px 4px 12px rgba(12, 12, 13, 0.2)',
+    40: '0px 8px 16px rgba(0, 0, 0, 0.25)',
+  },
 };
 
 export default palette;
