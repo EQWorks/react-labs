e16929a53207203b2e7434ccfaf946ee9fcaa807
hyx131 <selin.hyx@gmail.com>
2020-06-23 17:05:56 -0400

    card - card component with content/action as props

---
+++ src/card.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { makeStyles } from '@material-ui/core/styles'
+import MUICard from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: '40%',
+    padding: '5px',
+  }
+})
+
+const Card = ({ cardContent, cardAction }) => {
+  const classes = useStyles()
+  return (
+    <MUICard className={classes.root}>
+      <CardContent>
+        {cardContent}
+      </CardContent>
+      <CardActions>{cardAction}</CardActions>
+    </MUICard>
+  )
+}
+
+const propTypes = {
+  cardContent: PropTypes.any.isRequired,
+  cardAction: PropTypes.any,
+}
+
+Card.propTypes = propTypes
+export default Card
