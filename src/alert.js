50269e885af0c5bb9885473fdebb2948295f59fc
hyx131 <selin.hyx@gmail.com>
2020-06-26 22:08:29 -0400

    card - styled card component - add cardHeader and cardActions - include sm and md general size - add login and home sub-page card example

--- src/alert.js
+++ src/alert.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { makeStyles } from '@material-ui/core/styles'
 import MUIAlert from '@material-ui/lab/Alert'
 import AlertTitle from '@material-ui/lab/AlertTitle'
 
 
-const useStyles = makeStyles(() => ({
-  root: {
-    width: '100%',
-  },
-}))
-
-const Alert = ({ severity, message, header, ...props }) => {
-  const classes = useStyles()
+const Alert = ({ severity, message, header, width, height, ...props }) => {
+  const dimensions = { width, height }
 
   return (
-    <div className={classes.root}>
+    <div style={dimensions}>
       {header ? (
         <MUIAlert severity={severity} {...props}>
           <AlertTitle><strong>{header}</strong></AlertTitle>
@@ -33,10 +26,14 @@ Alert.propTypes = {
   severity: PropTypes.string.isRequired,
   message: PropTypes.string.isRequired,
   header: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
 }
 
 Alert.defaultProps = {
   header: '',
+  width: '100%',
+  height: '',
 }
 
 export default Alert
