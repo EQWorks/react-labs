797a735ebfa9fec5fba3b87a206057785097a883
DoParkEQ <do.park@eqworks.com>
2020-07-03 17:53:26 -0400

    carousel - slider control mechanism adopted

--- src/carousel.js
+++ src/carousel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import StyledCardContainer from "../src/styled-card-container";
 import {
   Avatar,
@@ -8,13 +8,24 @@ import {
   Link,
   Typography,
   makeStyles,
+  IconButton,
 } from "@material-ui/core";
 import CheckCircleOutlineRoundedIcon from "@material-ui/icons/CheckCircleOutlineRounded";
 import { data3 } from "../stories/data/cardInfo";
 import DynamicButton from "./dynamic-button";
 import { typography } from "@storybook/theming";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./css/carousel.css";
+import ArrowBackIosRoundedIcon from "@material-ui/icons/ArrowBackIosRounded";
+import ArrowForwardIosRoundedIcon from "@material-ui/icons/ArrowForwardIosRounded";
 
 const useStyles = makeStyles((theme) => ({
+  sliderControl: {
+    padding: "0px",
+    marginLeft: theme.spacing(2),
+  },
   textarea: {
     display: "flex",
     justifyContent: "space-between",
@@ -31,9 +42,10 @@ const useStyles = makeStyles((theme) => ({
   link: {
     display: "flex",
     alignItems: "flex-end",
+    paddingRight: theme.spacing(2),
   },
   container: {
-    marginRight: theme.spacing(3),
+    width: "50px",
   },
   header: {
     display: "flex",
@@ -44,7 +56,6 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     justifyContent: "space-between",
     height: "240px",
-    width: "400px",
   },
   tag: {
     display: "flex",
@@ -58,64 +69,90 @@ const useStyles = makeStyles((theme) => ({
 
 const Carousel = () => {
   const classes = useStyles();
-  const contents = ({ name, description, type, price, category }) => {
-    return (
-      <React.Fragment>
-        <Grid item>
-          <Grid item className={classes.tag}>
-            <Chip className={classes.chip} size="small" label={type} />
-            <Chip className={classes.chip} size="small" label={category} />
-          </Grid>
-        </Grid>
-        <Grid item>
-          <Grid item className={classes.header}>
-            <Typography variant="subtitle1">{name}</Typography>
-          </Grid>
-          <Typography variant="body1" gutterBottom>
-            {description}
-          </Typography>
-          <Typography variant="h6">{price}/mo</Typography>
-        </Grid>
-      </React.Fragment>
-    );
+  const customSlider = useRef();
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
   };
+
+
+  const gotoPrev = () => {
+    customSlider.current.slickPrev()
+  }
+
+  const gotoNext = () => {
+    customSlider.current.slickNext()
+  }
+
+  const topSection = (
+    <Grid container className={classes.textarea}>
+      <Grid item>
+        <Typography variant="subtitle1">Bundles</Typography>
+        <Typography variant="body1">
+          Bundles are the collection of segments and layers packaged based on a
+          specific persona.
+        </Typography>
+      </Grid>
+      <Grid item className={classes.link}>
+        <Link href="#" variant="body1" color="primary">
+          View all
+        </Link>
+        <IconButton
+          classes={{ root: classes.sliderControl }}
+          onClick={() => gotoPrev()}
+        >
+          <ArrowBackIosRoundedIcon fontSize="small" />
+        </IconButton>
+        <IconButton
+          classes={{ root: classes.sliderControl }}
+          onClick={() => gotoNext()}
+        >
+          <ArrowForwardIosRoundedIcon fontSize="small" />
+        </IconButton>
+      </Grid>
+    </Grid>
+  );
+
+  const contents = ({ name, description, type, price, category }) => (
+    <React.Fragment>
+      <Grid item>
+        <Grid item className={classes.tag}>
+          <Chip className={classes.chip} size="small" label={type} />
+          <Chip className={classes.chip} size="small" label={category} />
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid item className={classes.header}>
+          <Typography variant="subtitle1">{name}</Typography>
+        </Grid>
+        <Typography variant="body1" gutterBottom>
+          {description}
+        </Typography>
+        <Typography variant="h6">{price}/mo</Typography>
+      </Grid>
+    </React.Fragment>
+  );
 
   return (
     <div>
-      <Grid container className={classes.textarea}>
-        <Grid item>
-          <Typography variant="subtitle1">Bundles</Typography>
-          <Typography variant="body1">
-            Bundles are the collection of segments and layers packaged based on
-            a specific persona.
-          </Typography>
-        </Grid>
-        <Grid item className={classes.link}>
-          <Link
-            href="#"
-            variant="body1"
-            color="primary"
-          >
-            View all
-          </Link>
-        </Grid>
-      </Grid>
-      <div className={classes.wrapper}>
+      {topSection}
+      <Slider {...settings} className={classes.slider} ref={customSlider}>
         {data3.map((cardInfo, i) => (
-          <Grid key={i} className={classes.container}>
-            <StyledCardContainer
-              pattern={{
-                style: 3,
-                image: cardInfo.image,
-              }}
-            >
-              <CardContent className={classes.content}>
-                {contents(cardInfo)}
-              </CardContent>
-            </StyledCardContainer>
-          </Grid>
+          <StyledCardContainer
+            pattern={{
+              style: 3,
+              image: cardInfo.image,
+            }}
+          >
+            <CardContent className={classes.content}>
+              {contents(cardInfo)}
+            </CardContent>
+          </StyledCardContainer>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 };
