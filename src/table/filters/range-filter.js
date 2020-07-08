b01df5383655168ce2f5ac66fb2cb6ac967b743c
Tamires Lowande <tamires.lowande@gmail.com>
2020-07-08 18:46:23 -0400

    Table - adjust values displayed by range filter

--- src/table/filters/range-filter.js
+++ src/table/filters/range-filter.js
@@ -2,7 +2,6 @@ import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 
 import Slider from '@material-ui/core/Slider'
-import TextField from '@material-ui/core/TextField'
 import { makeStyles } from '@material-ui/core/styles'
 
 
@@ -11,7 +10,7 @@ function abbreviateNumber(value) {
   const suffixes = ['', 'k', 'm', 'b', 't']
   let newValue = value
   if (value >= 1000) {
-    const suffixNum = Math.floor(String(value).length / 3)
+    const suffixNum = Math.floor(String(Math.floor(value)).length / 3)
     let shortValue = '';
     for (var precision = 2; precision >= 1; precision--) {
       shortValue = (suffixNum !== 0 ? (value / Math.pow(1000, suffixNum)) : value)
@@ -25,6 +24,8 @@ function abbreviateNumber(value) {
       shortValue = shortValue.toFixed(1)
     }
     newValue = `${shortValue}${suffixes[suffixNum]}`
+  } else if (value % 1 != 0 && value > 1) { //to account for float numbers
+    newValue = Math.floor(value).toString()
   }
   return newValue;
 }
@@ -32,16 +33,17 @@ function abbreviateNumber(value) {
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '40ch',
-    padding: theme.spacing(1),
+    padding: theme.spacing(4, 2, 0, 2),
+    textAlign: 'center'
   },
   text: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    width: '18ch',
+    width: '16ch',
   },
 }))
 
-const RangeFilter = ({ column: { filterValue, preFilteredRows, setFilter, id } }) => {
+const RangeFilter = ({ column: { filterValue, preFilteredRows, setFilter, id, percentage } }) => {
   const classes = useStyles()
   const [min, max] = useMemo(() => {
     let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
@@ -54,9 +56,9 @@ const RangeFilter = ({ column: { filterValue, preFilteredRows, setFilter, id } }
   }, [id, preFilteredRows])
 
   return (
-    <div className={classes.root} onClick={(e) => { e.stopPropagation() }}>
+    <div className={classes.root} onClick={(e) => { e.stopPropagation() }} >
       <Slider
-        value={filterValue || [min, max]}
+        value={filterValue || [Math.ceil(max), Math.floor(min)]}
         onChange={(_, newValue) => {
           const [_min, _max] = newValue
           if (_min === min && _max === max) {
@@ -65,38 +67,13 @@ const RangeFilter = ({ column: { filterValue, preFilteredRows, setFilter, id } }
             setFilter(newValue)
           }
         }}
-        max={max}
-        min={min}
-        valueLabelDisplay="auto"
+        max={Math.ceil(max)}
+        min={Math.floor(min)}
+        valueLabelDisplay="on"
         aria-labelledby={`${id}-range-label`}
-        getAriaValueText={abbreviateNumber}
-        valueLabelFormat={abbreviateNumber}
-      />
-      <TextField
-        className={classes.text}
-        id={`${id}-range-min`}
-        label="min"
-        type="number"
-        variant="outlined"
-        size="small"
-        placeholder={min}
-        value={(filterValue || [])[0] || ''}
-        onChange={({ target: { value } }) => {
-          setFilter((old = []) => [value ? parseInt(value, 10) : undefined, old[1]])
-        }}
-      />
-      <TextField
-        className={classes.text}
-        id={`${id}-range-max`}
-        label="max"
-        type="number"
-        variant="outlined"
-        size="small"
-        placeholder={max}
-        value={(filterValue || [])[1] || ''}
-        onChange={({ target: { value } }) => {
-          setFilter((old = []) => [old[0], value ? parseInt(value, 10) : undefined])
-        }}
+        getAriaValueText={percentage ? (value) => (value*100) : abbreviateNumber}
+        valueLabelFormat={percentage ? (value) => (value*100) : abbreviateNumber}
+        step={max - min <= 1 ? 0.1 : 1}
       />
     </div>
   )
