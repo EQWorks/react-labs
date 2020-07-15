4f85b81a32e05329e93c21846ed800f656d8d0bf
DoParkEQ <do.park@eqworks.com>
2020-07-15 13:17:01 -0400

    Carousel - responsive sizing mechanism added

--- src/image-slider/carousel.js
+++ src/image-slider/carousel.js
@@ -16,6 +16,29 @@ const Carousel = ({ carouselContent, getRef }) => {
     arrows: false,
     slidesToShow: imagesToShow,
     slidesToScroll: imagesToShow,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: imagesToShow > 1 ? imagesToShow - 1 : 1,
+          slidesToScroll: imagesToShow > 1 ? imagesToShow - 1 : 1,
+        },
+      },
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: imagesToShow > 2 ? imagesToShow - 2 : 1,
+          slidesToScroll: imagesToShow > 2 ? imagesToShow - 2 : 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: imagesToShow > 3 ? imagesToShow - 3 : 1,
+          slidesToScroll: imagesToShow > 3 ? imagesToShow - 3 : 1,
+        }
+      },
+    ]
   };
 
   return (
