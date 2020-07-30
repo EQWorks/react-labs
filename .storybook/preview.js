e075fab005843b65b706d7f5bc70368244282633
Sam Sverko <sam.sverko@gmail.com>
2020-07-30 12:02:43 -0400

    Theme - Fix theme props not passing to makeStyles.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,12 +2,12 @@ import React from "react";
 import { addDecorator, configure } from "@storybook/react";
 import { ThemeProvider } from "@material-ui/core/styles";
 
-import LOCUSTheme from "../src/theme";
+import customTheme from '../src/theme/index';
 
 configure(require.context("../stories", true, /\.stories\.js$/), module);
 
-const GlobalWrapper = (storyFn) => {
-  return <ThemeProvider theme={LOCUSTheme}>{storyFn(LOCUSTheme)}</ThemeProvider>
-};
-
-addDecorator(GlobalWrapper);
+addDecorator(StoryFn => (
+  <ThemeProvider theme={customTheme}>
+    <StoryFn />
+  </ThemeProvider>
+))
