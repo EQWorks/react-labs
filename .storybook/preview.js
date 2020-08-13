14f6ff6089049073163b7f13c1c7b69480b369f6
Sam Sverko <sam.sverko@gmail.com>
2020-08-13 14:56:46 -0400

    Deps - Add new Storybook essentials dependency. - Update story file link from deprecated model.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,3 @@
-import { configure } from "@storybook/react";
-
-configure(require.context("../stories", true, /\.stories\.js$/), module);
+export const parameters = {
+  controls: { expanded: true },
+}
