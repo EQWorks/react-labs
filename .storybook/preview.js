5db13c402ddabcbaf1ab61c3d25c4d5938bc886f
Sam Sverko <sam.sverko@gmail.com>
2020-08-14 11:01:02 -0400

    Theme - Add Storybook ThemeProvider. - Remove redundant theme wrapping.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
+import React from 'react'
 import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { ThemeProvider } from '@material-ui/core/styles'
+
+import theme from '../src/theme/index'
 
 export const parameters = {
   controls: { expanded: true },
@@ -6,3 +10,11 @@ export const parameters = {
     viewports: MINIMAL_VIEWPORTS,
   },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
