6083fd6ad9a49b12ec4e4b05cb1b7b0994fa2db5
Sam Sverko <sam.sverko@gmail.com>
2020-09-18 15:07:47 -0400

    RefetchData - Begin building component.

--- stories/refetch-data.stories.js
+++ stories/refetch-data.stories.js
@@ -1,21 +1,74 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
-// import { makeStyles } from '@material-ui/core/styles'
+import { useQuery } from 'react-query'
+import { ReactQueryDevtools } from 'react-query-devtools'
+import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles'
+import RefreshIcon from '@material-ui/icons/Refresh'
 
-// const useStyles = makeStyles((theme) => {
-//   return {
-//     noSpacing: {
-//       padding: theme.spacing(0),
-//     },
-//   }
-// })
+// import PropTypes from 'prop-types'
+
+const useStyles = makeStyles((theme) => {
+  return {
+    container: {
+      alignItems: 'center',
+      boxSizing: 'border-box',
+      display: 'inline-flex',
+      height: '50px',
+      justifyContent: 'center',
+      padding: '5px',
+      width: '50px',
+    },
+    button: {
+      alignItems: 'center',
+      backgroundColor: theme.palette.secondary[900],
+      border: 'none',
+      borderRadius: '100%',
+      cursor: 'pointer',
+      display: 'inline-flex',
+      height: '100%',
+      justifyContent: 'center',
+      transition: 'background-color 0.25s ease-out',
+      width: '100%',
+      '&:hover': {
+        backgroundColor: theme.palette.secondary[700],
+      },
+      '&:active': {
+        backgroundColor: theme.palette.secondary[500],
+      },
+      '& svg': {
+        color: theme.palette.common.white,
+      },
+    },
+  }
+})
 
 const RefetchData = () => {
-  // const classes = useStyles()
+  // const [intervalMs] = useState(300000) // 5 minutes
+  // const cache = useQueryCache()
+  const classes = useStyles()
+  const { data, error, refetch, status } = useQuery('data', async () => {
+    const { data } = await axios.get(
+      'https://api.github.com/repos/tannerlinsley/react-query',
+    )
+    console.log(data)
+    return data
+  })
+
+  if (status === 'loading') return <h1>Loading...</h1>
+  if (status === 'error') return <span>Error: {error.message}</span>
 
   return (
     <div>
-      <p>Hello there</p>
+      <h1>Test hook</h1>
+      <div className={classes.container}>
+        <button className={classes.button} onClick={() => refetch()}><RefreshIcon /></button>
+      </div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>
+      <strong>✨ {data.stargazers_count}</strong>
+      <strong>🍴 {data.forks_count}</strong>
+      <ReactQueryDevtools initialIsOpen />
     </div>
   )
 }
