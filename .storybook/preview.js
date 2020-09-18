70c86b8cea47dc1a0dffed1050ee5a001566070a
Sam Sverko <sam.sverko@gmail.com>
2020-09-18 13:53:28 -0400

    Deps - Add axios dep. - Add react-query-devtools to Storybook app wrapper.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { ReactQueryDevtools } from 'react-query-devtools'
 
 import { ThemeProvider } from '../src/index'
 
 export const decorators = [
   (Story) => (
     <ThemeProvider>
+      <ReactQueryDevtools initialIsOpen />
       <Story />
     </ThemeProvider>
   ),
