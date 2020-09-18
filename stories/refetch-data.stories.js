242c1318291b40b0bdd9bfed105f38525f217141
Sam Sverko <sam.sverko@gmail.com>
2020-09-18 11:24:25 -0400

    RefetchData - Begin story to build component.

---
+++ stories/refetch-data.stories.js
@@ -0,0 +1,30 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+// import { makeStyles } from '@material-ui/core/styles'
+
+// const useStyles = makeStyles((theme) => {
+//   return {
+//     noSpacing: {
+//       padding: theme.spacing(0),
+//     },
+//   }
+// })
+
+const RefetchData = () => {
+  // const classes = useStyles()
+
+  return (
+    <div>
+      <p>Hello there</p>
+    </div>
+  )
+}
+
+export default {
+  title: 'Lab/Refetch Data',
+  component: RefetchData,
+  args: {},
+  argTypes: {},
+}
+
+export const Default = RefetchData
