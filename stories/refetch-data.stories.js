a140bbb76484e5c6ba01b1dce08b5894f96e0613
Sam Sverko <sam.sverko@gmail.com>
2020-09-21 14:13:15 -0400

    RefetchData - Update test API URL. [build]

--- stories/refetch-data.stories.js
+++ stories/refetch-data.stories.js
@@ -6,7 +6,7 @@ export default {
   title: 'Lab/Refetch Data',
   component: RefetchData,
   args: {
-    fetchUrl: 'http://slowwly.robertomurray.co.uk/delay/3000/url/https://jsonplaceholder.typicode.com/users/1',
+    fetchUrl: 'https://jsonplaceholder.typicode.com/users/1',
   },
   argTypes: {},
 }
