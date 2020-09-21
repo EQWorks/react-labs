f95926df2a1651c98f2a59a85658529778178a68
Sam Sverko <sam.sverko@gmail.com>
2020-09-21 15:15:46 -0400

    RefetchData - Remove get-data functionality. - Storybook - Remove react-query devtools.

--- stories/refetch-data.stories.js
+++ stories/refetch-data.stories.js
@@ -5,9 +5,7 @@ import { RefetchData } from '../src/index'
 export default {
   title: 'Lab/Refetch Data',
   component: RefetchData,
-  args: {
-    fetchUrl: 'https://jsonplaceholder.typicode.com/users/1',
-  },
+  args: {},
   argTypes: {},
 }
 
