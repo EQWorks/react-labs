e586a09affbce3cf3310c5b93db54c961574c27d
Sam Sverko <sam.sverko@gmail.com>
2020-09-22 10:07:13 -0400

    RefetchData - Handle mouse and key press. - Add sample story methods to visualize fetch data statuses. - Provide a11y visuals for focus and hover states.

--- src/refetch-data.js
+++ src/refetch-data.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import RefreshIcon from '@material-ui/icons/Refresh'
@@ -33,21 +33,54 @@ const useStyles = makeStyles((theme) => {
       justifyContent: 'center',
       transition: 'background-color 0.25s ease-out',
       width: '100%',
-      '&:hover': {
-        backgroundColor: theme.palette.secondary[700],
-      },
       '&:active': {
         backgroundColor: theme.palette.secondary[500],
       },
+      '&:focus': {
+        backgroundColor: theme.palette.secondary[500],
+        border: `2px solid ${theme.palette.primary.main}`,
+        outline: 'none',
+      },
+      '&:hover': {
+        backgroundColor: theme.palette.secondary[700],
+      },
       '& svg': {
         color: theme.palette.common.white,
+        outline: 'none',
       },
     },
   }
 })
 
-const RefetchData = ({ status, update }) => {
+const RefetchData = ({ fetchDataFunction, status }) => {
+  const [hideAlert, setHideAlert] = useState(false)
+  const buttonRef = useRef()
   const classes = useStyles()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (status === 'error') {
+        setHideAlert(true)
+      }
+    }, 2000)
+    return () => {
+      setHideAlert(false)
+      clearTimeout(timer)
+    }
+  }, [status])
+
+  const handleClick = () => {
+    fetchDataFunction()
+    buttonRef.current.blur()
+  }
+
+  const handleKey = (event) => {
+    if (event.keyCode === 13 || event.keyCode === 32) {
+      fetchDataFunction()
+    } else {
+      buttonRef.current.blur()
+    }
+  }
 
   return (
     <>
@@ -69,8 +102,16 @@ const RefetchData = ({ status, update }) => {
           width="100%"
         />
       )}
-      <div className={classes.container}>
-        <button className={classes.button} onClick={update}><RefreshIcon /></button>
+      <div className={classes.container} tabIndex='-1'>
+        <button
+          aria-label='Refresh data'
+          className={classes.button}
+          onKeyDown={handleKey}
+          onMouseUp={handleClick}
+          ref={buttonRef}
+        >
+          <RefreshIcon tabIndex='-1' />
+        </button>
       </div>
     </>
   )
@@ -80,11 +121,11 @@ RefetchData.propTypes = {
   /**
     * The request status.
   */
-  status: PropTypes.oneOf(['loading', 'error']),
+  status: PropTypes.oneOf(['error', 'loading', 'none']),
   /**
     * The request method.
   */
-  update: PropTypes.function,
+  fetchDataFunction: PropTypes.func,
 }
 
 export default RefetchData
