f12eea35a3d68f2d41121989af422fe9ed3bb3ae
Sam Sverko <sam.sverko@gmail.com>
2020-10-09 14:50:36 -0400

    Switch - Update story to include input label.

--- stories/switch.stories.js
+++ stories/switch.stories.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-// import { FormControlLabel } from '@material-ui/core'
 
-import { Switch } from '../src/index'
+import { FormControlLabel, Switch } from '../src/index'
 
 export default {
   title: 'Lab/Switch',
   component: Switch,
-  args: {},
+  args: {
+    label: 'Item',
+  },
   argTypes: {
     checked: {
       control: null,
@@ -38,4 +39,39 @@ const Template = (args) => {
   )
 }
 
+const TemplateControlLabel = (args) => {
+  const [state, setState] = React.useState({
+    checkedA: false,
+  })
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked })
+  }
+
+  return (
+    <FormControlLabel
+      control={<Switch checked={state.checkedA} onChange={handleChange} name='checkedA' {...args} />}
+      label={args.label}
+    />
+  )
+}
+
 export const Default = Template.bind({})
+
+// ===
+
+export const ControlLabel = TemplateControlLabel.bind({})
+
+ControlLabel.argTypes = {
+  label: {
+    description: 'The text to be used in an enclosing label element.',
+    control: {
+      type: 'text',
+    },
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: 'Hello' },
+    },
+    type: { name: 'string', required: false },
+  },
+}
