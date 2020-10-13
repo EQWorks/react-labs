18a48afae7b6bd5880077023719af3713a9b2639
Sam Sverko <sam.sverko@gmail.com>
2020-10-13 09:32:18 -0400

    Checkbox - Extract checkmark SVG for easier use.

--- src/checkbox.js
+++ src/checkbox.js
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import MUICheckbox from '@material-ui/core/Checkbox'
 
-const CHECKMARK =
-  'url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 16 16\'%3E%3Cpath' +
-  ' fill-rule=\'evenodd\' clip-rule=\'evenodd\' d=\'M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 ' +
-  '1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z\' fill=\'%23fff\'/%3E%3C/svg%3E")'
+const checkSVG = `
+  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'>
+    <path 
+      fill-rule='evenodd'
+      clip-rule='evenodd'
+      d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z'
+      fill='#fff'
+    />
+  </svg>
+`
+const CHECKMARK = `url("data:image/svg+xml;charset=utf-8,${encodeURIComponent(checkSVG)}")`
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -63,7 +70,7 @@ const Checkbox = ({ checked, disabled, ...props }) => {
   }
   return (
     <MUICheckbox
-      data-testid='checkbox'
+      data-testid="checkbox"
       disabled={disabled}
       disableRipple
       checked={checkedState}
@@ -80,16 +87,16 @@ const Checkbox = ({ checked, disabled, ...props }) => {
 
 Checkbox.propTypes = {
   /**
-    * If `true`, the component is checked.
-  */
+   * If `true`, the component is checked.
+   */
   checked: PropTypes.bool,
   /**
-    * If `true`, the checkbox will be disabled.
-  */
+   * If `true`, the checkbox will be disabled.
+   */
   disabled: PropTypes.bool,
   /**
-    * The event source of the callback. You can pull out the new checked state by accessing `event.target.checked` (boolean).
-  */
+   * The event source of the callback. You can pull out the new checked state by accessing `event.target.checked` (boolean).
+   */
   onChange: PropTypes.func,
 }
 
