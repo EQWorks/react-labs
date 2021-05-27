b703e50fc083384a47c743e21e67a7b0b4ac3520
hyx131 <selin.hyx@gmail.com>
2021-05-27 16:27:32 -0400

    selector/tree - add sample data for tree component

---
+++ stories/data/tree-menu.js
@@ -0,0 +1,283 @@
+export const _menuOptions = [
+  {
+    id: 'grains',
+    name: 'Grains',
+    children: [
+      {
+        id: 'whole_grains',
+        name: 'Whole Grains',
+        children: [
+          {
+            id: 'brown_rice',
+            name: 'Brown Rice',
+            children: [],
+          },
+          {
+            id: 'buckwheat',
+            name: 'Buckwheat',
+            children: [],
+          },
+          {
+            id: 'oatmeal',
+            name: 'Oatmeal',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'other_grains',
+        name: 'Others',
+        children: [
+          {
+            id: 'corn_bread',
+            name: 'Cornbread',
+            children: [],
+          },
+          {
+            id: 'crackers',
+            name: 'Crackers',
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'vegetables',
+    name: 'Vegetables',
+    children: [
+      {
+        id: 'green_veg',
+        name: 'Dark Green Vegetables',
+        children: [
+          {
+            id: 'bok_choy',
+            name: 'Bok Choy',
+            children: [],
+          },
+          {
+            id: 'broccoli',
+            name: 'Broccoli',
+            children: [],
+          },
+          {
+            id: 'kale',
+            name: 'Kale',
+            children: [],
+          },
+          {
+            id: 'spinach',
+            name: 'Spinach',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'red_orange_veg',
+        name: 'Red & Orange Vegetables',
+        children: [
+          {
+            id: 'carrots',
+            name: 'Carrots',
+            children: [],
+          },
+          {
+            id: 'pumpkin',
+            name: 'Pumpkin',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'starchy_veg',
+        name: 'Starchy Vegetables',
+        children: [
+          {
+            id: 'corn',
+            name: 'Corn',
+            children: [],
+          },
+          {
+            id: 'green_peas',
+            name: 'Green Peas',
+            children: [],
+          },
+          {
+            id: 'potato',
+            name: 'Potato',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'other_veg',
+        name: 'Others',
+        children: [
+          {
+            id: 'asparagus',
+            name: 'Asparagus',
+            children: [],
+          },
+          {
+            id: 'avocado',
+            name: 'Avocado',
+            children: [],
+          },
+          {
+            id: 'cauliflower',
+            name: 'Cauliflower',
+            children: [],
+          },
+          {
+            id: 'eggplant',
+            name: 'Eggplant',
+            children: [],
+          },
+          {
+            id: 'mushrooms',
+            name: 'Mushrooms',
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'protein',
+    name: 'Protein',
+    children: [
+      {
+        id: 'meats',
+        name: 'Meats',
+        children: [
+          {
+            id: 'beef',
+            name: 'Beef',
+            children: [],
+          },
+          {
+            id: 'pork',
+            name: 'Pork',
+            children: [],
+          },
+          {
+            id: 'lamb',
+            name: 'Lamb',
+            children: [],
+          },
+          {
+            id: 'ham',
+            name: 'Ham',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'poultry',
+        name: 'Poultry',
+        children: [
+          {
+            id: 'chicken',
+            name: 'Skinless Chicken',
+            children: [],
+          },
+          {
+            id: 'turkey',
+            name: 'Turkey',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'fish',
+        name: 'Fish',
+        children: [
+          {
+            id: 'salmon',
+            name: 'Salmon',
+            children: [],
+          },
+          {
+            id: 'clams',
+            name: 'Clams',
+            children: [],
+          },
+          {
+            id: 'lobster',
+            name: 'Lobster',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'beans',
+        name: 'Beans',
+        children: [
+          {
+            id: 'cooked',
+            name: 'Cooked Beans',
+            children: [
+              {
+                id: 'black_beans',
+                name: 'Black Beans',
+                children: [],
+              },
+              {
+                id: 'lentils',
+                name: 'Lentils',
+                children: [],
+              },
+            ],
+          },
+          {
+            id: 'refried',
+            name: 'Refried Beans',
+            children: [],
+          },
+          {
+            id: 'tofu',
+            name: 'Tofu',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'seeds',
+        name: 'Nuts & Seeds',
+        children: [
+          {
+            id: 'peanuts',
+            name: 'Peanuts',
+            children: [],
+          },
+          {
+            id: 'almonds',
+            name: 'Almonds',
+            children: [],
+          },
+          {
+            id: 'sunflower_seeds',
+            name: 'Sunflower Seeds',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'eggs',
+        name: 'Eggs',
+        children: [
+          {
+            id: 'chicken_eggs',
+            name: 'Chicken Eggs',
+            children: [],
+          },
+          {
+            id: 'duck_eggs',
+            name: 'Duck Eggs',
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+]
